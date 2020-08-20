90422dd039c14741838fe9d4ca27f9bf5f037193
Naveen Naidu <naveennaidu479@gmail.com>
2020-08-20 23:36:58 +0530

    Initial state drawing
    We now have a QuoteBox and a Editor Field on the
    design.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,36 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Lib
     ( tui
     ) where
 
-
+import Lens.Micro
+import Lens.Micro.TH
 import Brick
-import Brick.Widgets.Center
 import Brick.Widgets.Border
 import Brick.Widgets.Border.Style
 import qualified Brick.Widgets.Edit as E
+import qualified Brick.Widgets.Center as C
+
+import Brick.Widgets.Core
+  ( (<+>)
+  , (<=>)
+  , hLimit
+  , vLimit
+  , str
+  )
 
 -- | Named resoureced
-type Name = ()
+data Name = QuoteBox | TypeBox 
+            deriving (Ord, Show, Eq)
+
+-- | The state of the game
+-- | We'll make a lens out of it, so that we have getters and setter
+data TuiState =
+    TuiState {  _quoteBox :: String
+             ,  _typeBox  :: E.Editor String Name
+             }
+
+makeLenses ''TuiState
 
 -- | The main application to draw the UI
 tui :: IO ()
@@ -19,12 +39,6 @@ tui = do
   endState <- defaultMain tuiApp initialState
   putStrLn "Hello"
 
--- | The state of the game
-data TuiState =
-    TuiState {  _quoteBox :: String
-             ,  _typeBox  :: E.Editor String Name
-
-             } deriving (Show)
 
 -- | The initial config to out APP, specifying various functions
 -- | appDraw: Turns the current app state into a list of layers of type Widget
@@ -40,11 +54,23 @@ tuiApp =
 
 -- | Initial state of the APP
 buildInitialState :: IO TuiState
-buildInitialState = undefined
+buildInitialState = do
+    pure TuiState {_quoteBox = "Quote to type", _typeBox = E.editor TypeBox Nothing ""}
 
 -- | Change TuiState to drawable Widgets. This does the actual drawing
+-- | `<=>` is a sugar for Vertical box Layout. Put widgets one above another
+-- | `ts^.typeBox` means get the `_typeBox` constructor from TuiState
+
+-- TODO: Add Focus to Editor Box to get the cursor where you can type
 drawTui :: TuiState -> [Widget Name]
-drawTui = undefined
+drawTui ts = [ui]
+    where
+        typeBox' = E.renderEditor (str . unlines) False (ts^.typeBox)
+        ui = withBorderStyle unicode $
+                borderWithLabel (str "TypeRacer") $ 
+                    vBox [str (ts^.quoteBox), fill ' ', hBorder] <=>
+                     (hLimit 30 $ vLimit 5 typeBox')
+
 
 -- | Handle the events. This is where Keyboard events will be captured.
 handleTuiEvent :: TuiState -> BrickEvent n e -> EventM n (Next TuiState)
