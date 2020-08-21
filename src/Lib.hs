17c14394acd74939d23db4f0196b6b5b3d0c5211
Naveen Naidu <naveennaidu479@gmail.com>
2020-08-21 21:23:32 +0530

    Add Focus to Edit Text Add cursor to Focus Ring

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,8 +8,11 @@ import Lens.Micro.TH
 import Brick
 import Brick.Widgets.Border
 import Brick.Widgets.Border.Style
-import qualified Brick.Widgets.Edit as E
-import qualified Brick.Widgets.Center as C
+
+import qualified Brick.Focus                as F
+import qualified Brick.Widgets.Edit         as E
+import qualified Brick.Widgets.Center       as C
+import qualified Brick.Types                as T
 
 import Brick.Widgets.Core
   ( (<+>)
@@ -26,8 +29,9 @@ data Name = QuoteBox | TypeBox
 -- | The state of the game
 -- | We'll make a lens out of it, so that we have getters and setter
 data TuiState =
-    TuiState {  _quoteBox :: String
-             ,  _typeBox  :: E.Editor String Name
+    TuiState {  _focusRing  :: F.FocusRing Name
+             ,  _quoteBox   :: String
+             ,  _typeBox    :: E.Editor String Name
              }
 
 makeLenses ''TuiState
@@ -46,7 +50,7 @@ tuiApp :: App TuiState e Name
 tuiApp =
   App
     { appDraw = drawTui
-    , appChooseCursor = showFirstCursor
+    , appChooseCursor = appCursor
     , appHandleEvent = handleTuiEvent
     , appStartEvent = pure
     , appAttrMap = const $ attrMap mempty []
@@ -55,17 +59,25 @@ tuiApp =
 -- | Initial state of the APP
 buildInitialState :: IO TuiState
 buildInitialState = do
-    pure TuiState {_quoteBox = "Quote to type", _typeBox = E.editor TypeBox Nothing ""}
+    pure TuiState { _focusRing =  F.focusRing [TypeBox]
+                  , _quoteBox = "Quote to type"
+                  , _typeBox = E.editor TypeBox Nothing ""
+                  }
+
+-- | The cursor as I understand is used to send the information from widget back to program, think of it like dbms cursor
+appCursor :: TuiState -> [T.CursorLocation Name] -> Maybe (T.CursorLocation Name)
+appCursor = F.focusRingCursor (^.focusRing)
 
 -- | Change TuiState to drawable Widgets. This does the actual drawing
 -- | `<=>` is a sugar for Vertical box Layout. Put widgets one above another
 -- | `ts^.typeBox` means get the `_typeBox` constructor from TuiState
+-- | FocusRing helps to keep focus on the Named Resource. This also gives cursor to the edit box
 
 -- TODO: Add Focus to Editor Box to get the cursor where you can type
 drawTui :: TuiState -> [Widget Name]
 drawTui ts = [ui]
     where
-        typeBox' = E.renderEditor (str . unlines) False (ts^.typeBox)
+        typeBox' = F.withFocusRing (ts^.focusRing)(E.renderEditor (str . unlines)) (ts^.typeBox)
         ui = withBorderStyle unicode $
                 borderWithLabel (str "TypeRacer") $ 
                     vBox [str (ts^.quoteBox), fill ' ', hBorder] <=>
