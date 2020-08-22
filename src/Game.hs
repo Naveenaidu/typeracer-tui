10f04ead0e15e754464c8b7117a45cb9d5e88983
Naveen Naidu <naveennaidu479@gmail.com>
2020-08-22 23:30:04 +0530

    Game.hs: Update functions to use Lens Add helper Functions
    Lib.hs:
    Add Game state to TUI state
    Add handleChar function
    Initial Commit without focus

--- src/Game.hs
+++ src/Game.hs
@@ -1,24 +1,84 @@
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Game where
+module Game  where
 
 import Data.Time  (UTCTime, diffUTCTime)
-import Data.Text
+import Lens.Micro
+import Lens.Micro.TH
+import           Data.Char  (isSpace)
 
-data State = State
+import qualified Data.Text  as T
+
+
+newtype Quote = Quote { unQuote :: T.Text } deriving (Show)
+newtype Input = Input { unInput :: T.Text } deriving (Show)
+
+data Game = Game
   {
-    quote     :: Text
-  , input     :: Text
-  , startTime :: Maybe UTCTime
-  , endTime   :: Maybe UTCTime
-  , hits      :: Int 
-  , strokes   :: Int
+    _quote     :: Quote
+  , _input     :: Input
+  , _startTime :: Maybe UTCTime
+  , _endTime   :: Maybe UTCTime
+  , _hits      :: Int 
+  , _strokes   :: Int
   } deriving (Show)
+
+makeLenses ''Game
+
+initialState :: String -> Game
+initialState t =
+  Game
+    { _quote = Quote (T.pack t)
+    , _input = Input (T.pack " ")
+    , _startTime = Nothing
+    , _endTime = Nothing
+    , _strokes = 0
+    , _hits = 0
+    }
 
 -- | The character can either be hit correctly or missed
 data Character = 
     Hit Char
   | Miss Char
 
+type Line = [Character]
+
+-- | Take Quote and Input String and give back the list of character
+-- | quote' are the characters from Quote that we match with the input string
+character :: Quote -> Input -> [Character]
+character (Quote quote) (Input input) = map mkChar $ T.zip quote' input
+  where quote' = T.take (T.length input) quote
+        mkChar (q,i)
+          | q == i    = Hit i
+          | otherwise = Miss i
+
+-- | Check if the Input at time `t` is equal to that much part of quote
+isErrorFree :: Game -> Bool
+isErrorFree st = input' `T.isPrefixOf` quote'
+  where quote' = st^.quote & unQuote
+        input'  = st^.input & unInput
+
+-- | The game is complete when input is equal to quote
+isComplete :: Game -> Bool
+isComplete st = quote' == input'
+  where quote' = st^.quote & unQuote
+        input'  = st^.input & unInput
+
+-- | TODO: use appendInput instead of newInput
+applyChar :: Char -> Game -> Game
+applyChar char st = st & (strokes +~ 1) . (input .~ newInput) . (hits +~ isErrorFree')
+  where appendInput = Input <$>  (T.cons char) . unInput
+        newInput = Input $ T.snoc (unInput (st ^. input)) char
+        isErrorFree' = if isErrorFree st then 1 else 0
+
+applyBackSpace :: Game -> Game
+applyBackSpace =  undefined
+
+applyBackSpaceWord :: Game -> Game
+applyBackSpaceWord = undefined
+
+applyWhiteSpace :: Game -> Game
+applyWhiteSpace = undefined
 
 test :: IO ()
 test = putStrLn "Hello"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( tui
     ) where
@@ -24,26 +26,49 @@ import qualified Brick.Main                 as M
 import qualified Graphics.Vty               as V
 
 import Game
+
 -- | Named resoureced
 data Name = QuoteBox | TypeBox 
             deriving (Ord, Show, Eq)
 
 -- | The state of the game
 -- | We'll make a lens out of it, so that we have getters and setter
+-- | QUESTION: Do we really need a EDIT box? ?
 data TuiState =
-    TuiState {  _focusRing  :: F.FocusRing Name
+    TuiState {  _game       :: Game 
              ,  _quoteBox   :: String
              ,  _typeBox    :: E.Editor String Name
-             }
+             } deriving (Show)
 
 makeLenses ''TuiState
+
+-- | An attribute for HIT, Use this attribute when you have HIT action
+hitAttrName :: AttrName 
+hitAttrName = attrName "Hit"
+
+missAttrName :: AttrName
+missAttrName = attrName "Miss"
+
+-- | Draw the character, If Miss color the character with RED
+-- | str :: String -> Widget n
+drawCharacter :: Character -> Widget ()
+drawCharacter (Hit c)  = withAttr hitAttrName $ str [c]
+drawCharacter (Miss c) = withAttr missAttrName $ str [c]
+
+drawLine :: Line -> Widget ()
+drawLine [] = str " "
+drawLine line = foldl1 (<+>) $ map drawCharacter line 
+
+-- | Takes the state, Creates the characters and draws the line
+drawText :: TuiState -> Widget ()
+drawText st = undefined
 
 -- | The main application to draw the UI
 tui :: IO ()
 tui = do
   initialState <- buildInitialState
   endState <- defaultMain tuiApp initialState
-  putStrLn $ unlines $ E.getEditContents $ endState^.typeBox
+  putStrLn $ show endState 
 
 -- | The initial config to out APP, specifying various functions
 -- | appDraw: Turns the current app state into a list of layers of type Widget
@@ -51,7 +76,7 @@ tuiApp :: App TuiState e Name
 tuiApp =
   App
     { appDraw = drawTui
-    , appChooseCursor = appCursor
+    , appChooseCursor = showFirstCursor
     , appHandleEvent = handleTuiEvent
     , appStartEvent = pure
     , appAttrMap = const $ attrMap mempty []
@@ -60,45 +85,42 @@ tuiApp =
 -- | Initial state of the APP
 buildInitialState :: IO TuiState
 buildInitialState = do
-    pure TuiState { _focusRing =  F.focusRing [TypeBox]
+    pure TuiState { _game = initialState "Hello"
                   , _quoteBox = "Quote to type"
                   , _typeBox = E.editor TypeBox Nothing ""
                   }
 
 -- | The cursor as I understand is used to send the information from widget back to program, think of it like dbms cursor
-appCursor :: TuiState -> [CursorLocation Name] -> Maybe (CursorLocation Name)
-appCursor = F.focusRingCursor (^.focusRing)
+-- appCursor :: TuiState -> [CursorLocation Name] -> Maybe (CursorLocation Name)
+-- appCursor = F.focusRingCursor (^.focusRing)
 
 -- | Change TuiState to drawable Widgets. This does the actual drawing
 -- | `<=>` is a sugar for Vertical box Layout. Put widgets one above another
 -- | `ts^.typeBox` means get the `_typeBox` constructor from TuiState
 -- | FocusRing helps to keep focus on the Named Resource. This also gives cursor to the edit box
-
--- TODO: Add Focus to Editor Box to get the cursor where you can type
 drawTui :: TuiState -> [Widget Name]
 drawTui ts = [ui]
     where
-        typeBox' = F.withFocusRing (ts^.focusRing)(E.renderEditor (str . unlines)) (ts^.typeBox)
+        typeBox' =E.renderEditor (str . unlines) False (ts^.typeBox)
         ui = withBorderStyle unicode $
                 borderWithLabel (str "TypeRacer") $ 
                     vBox [str (ts^.quoteBox), fill ' ', hBorder] <=>
                      (hLimit 30 $ vLimit 5 typeBox')
 
 
+handleChar :: Char -> TuiState -> EventM Name (Next TuiState)
+handleChar char ts = M.continue $ ts & game %~ (applyChar char)
+
 -- | Handle the events. This is where Keyboard events will be captured.
 -- | Continue takes a updated state and applies it
 -- | `%~` modifies the value of a field. Like a setter
--- | `&` 
+-- | `&` is the reverse of `$` ie. You give the argument first and then the function. Eg: num & add == add $ num
 handleTuiEvent :: TuiState -> BrickEvent Name e -> EventM Name (Next TuiState)
 handleTuiEvent ts (VtyEvent ev) = 
     case ev of
         V.EvKey V.KEsc [] -> M.halt ts
-        V.EvKey (V.KChar '\t') [] -> M.continue $ ts & focusRing %~ F.focusNext
-        V.EvKey V.KBackTab [] -> M.continue $ ts & focusRing %~ F.focusPrev
-
-        -- For any other character get the currently-focused name in the ring
-        _ -> M.continue =<< case F.focusGetCurrent (ts^.focusRing) of
-                Just TypeBox -> T.handleEventLensed ts typeBox E.handleEditorEvent ev
-                Nothing      -> return ts
+        -- V.EvKey (V.KChar '\t') [] -> M.continue $ ts & focusRing %~ F.focusNext
+        -- V.EvKey V.KBackTab [] -> M.continue $ ts & focusRing %~ F.focusPrev
+        V.EvKey (V.KChar c) [] -> handleChar c ts
 
 handleTuiEvent ts _ = M.continue ts
